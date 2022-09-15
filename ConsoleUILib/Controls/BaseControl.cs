96b305504b00a968841324765a299b9a164a1b22
GoldenretriverYT <49924528+GoldenretriverYT@users.noreply.github.com>
2022-09-15 17:01:38 +0200

    Add controls base

---
+++ ConsoleUILib/Controls/BaseControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUILib.Controls
+{
+    public abstract class BaseControl
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public virtual void DrawControl()
+        {
+
+        }
+    }
+}
--- ConsoleUILib/Window/CustomWindow.cs
+++ ConsoleUILib/Window/CustomWindow.cs
@@ -1,4 +1,5 @@
-﻿using ConsoleUILib.Exceptions;
+﻿using ConsoleUILib.Controls;
+using ConsoleUILib.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,14 +8,45 @@ using System.Threading.Tasks;
 
 namespace ConsoleUILib.Window
 {
-    public class EmptyWindow : BaseWindow
+    public class CustomWindow : BaseWindow
     {
-        public EmptyWindow(int x, int y, int w, int h)
+        public List<BaseControl> Controls { get; init; }
+
+        public CustomWindow(int x, int y, int w, int h)
         {
+            this.Controls = new();
             this.X = x;
             this.Y = y;
             this.Width = w;
             this.Height = h;
+        }
+
+        public void AddControl(BaseControl control)
+        {
+            this.Controls.Add(control);
+        }
+
+        public void FindNextInteractable(int lastIdx, out BaseControl outCtrl, out int outIdx)
+        {
+            int idx = lastIdx + 1;
+
+            foreach(BaseControl control in Controls.Skip(lastIdx))
+            {
+                if(control is InteractableControl)
+                {
+                    outCtrl = control;
+                    outIdx = idx; 
+                }
+                idx++;
+            }
+
+            if (lastIdx != 0)
+                FindNextInteractable(0, out outCtrl, out outIdx);
+            else
+            {
+                outCtrl = null;
+                outIdx = 0;
+            }
         }
     }
 }
